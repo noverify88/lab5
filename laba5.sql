099e2da1b8b4296c3240369d024e5dbc2dd1d609
Erken <nover2020@bk.ru>
2025-11-24 14:51:45 +0900

    fixed good commit

--- laba5.sql
+++ laba5.sql
@@ -9,7 +9,7 @@ name varchar(2048) not null
 short_name varchar(64) unique not null
 year integer  not null
 institute_id integer not null
-}
+};
 
 create table students{
 id serial primary key
@@ -17,4 +17,4 @@ surname varchar(512) not null
 name varchar(512) not null
 father_name varchar(512)
 group_id integer not null
-}
+};
