7441789bb88f667c1a361abecdf57d56bbf4b40f
Erken <nover2020@bk.ru>
2025-11-24 15:02:55 +0900

    fixed2x good commit

--- laba5.sql
+++ laba5.sql
@@ -1,20 +1,20 @@
 CREATE TABLE institutes {
-id serial primary key
-name varchar(1024) not null
+	id serial primary key,
+	name varchar(1024) not null
 };
 
 create table groups{
-id serial primary key
-name varchar(2048) not null
-short_name varchar(64) unique not null
-year integer  not null
-institute_id integer not null
+	id serial primary key,
+	name varchar(2048) not null,
+	short_name varchar(64) unique not null,
+	year integer  not null,
+	institute_id integer not null
 };
 
 create table students{
-id serial primary key
-surname varchar(512) not null
-name varchar(512) not null
-father_name varchar(512)
-group_id integer not null
+	id serial primary key,
+	surname varchar(512) not null,
+	name varchar(512) not null,
+	father_name varchar(512),
+	group_id integer not null
 };
